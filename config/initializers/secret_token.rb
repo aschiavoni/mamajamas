b9fbc27baaed39282a0c420a90fec70137c55e6b
ryane <ryanesc@gmail.com>
2012-10-02 23:39:45 -0400

    initial rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mamajamas::Application.config.secret_token = '558de5bd71d996dc4c1c347be15e9589d2c23c5871e734b85e3a1c72c7c43bd7f2f91237debd132388a0f1280deb6151db5151b82b77a39c541c58a09f8aa343'
