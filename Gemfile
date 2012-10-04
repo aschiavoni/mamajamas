2b907eb4a13c233f0ab519bd354d4f8bbdfe9acd
ryane <ryanesc@gmail.com>
2012-10-04 00:14:59 -0400

    improve test output

--- Gemfile
+++ Gemfile
@@ -48,4 +48,5 @@ end
 
 group :test do
   gem 'minitest-rails-capybara'
+  gem 'turn', :require => false
 end
--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -10,6 +10,22 @@ require "minitest/rails/capybara"
 # Uncomment if you want awesome colorful output
 # require "minitest/pride"
 
+# better test output via turn
+Turn.config do |c|
+  # use one of output formats:
+  # :outline  - turn's original case/test outline mode [default]
+  # :progress - indicates progress with progress bar
+  # :dotted   - test/unit's traditional dot-progress mode
+  # :pretty   - new pretty reporter
+  # :marshal  - dump output as YAML (normal run mode only)
+  # :cue      - interactive testing
+  c.format  = :outline
+  # turn on invoke/execute tracing, enable full backtrace
+  c.trace   = true
+  # use humanized test names (works only with :outline format)
+  c.natural = true
+end
+
 class MiniTest::Rails::ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
   fixtures :all
