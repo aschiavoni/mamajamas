8831f2dc52e5d4701a74b28741729225293299d9
ryane <ryanesc@gmail.com>
2012-10-05 12:06:08 -0400

    initial User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Mamajamas::Application.routes.draw do
+  devise_for :users, path_names: { sign_in: "login", sign_out: "logout" }
   get '/robots.txt' => 'robots#show'
   root :to => 'home#index'
 end
