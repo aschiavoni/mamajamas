759c97d27ed4e979095690db3e0d74a2efb771ce
ryane <ryanesc@gmail.com>
2012-10-05 16:45:14 -0400

    active signup and login links

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Mamajamas::Application.routes.draw do
-  devise_for :users, path_names: { sign_in: "login", sign_out: "logout" }
+  devise_for :users, path_names: { sign_in: "login", sign_out: "logout", sign_up: "signup" }
   get '/robots.txt' => 'robots#show'
   root :to => 'home#index'
 end
