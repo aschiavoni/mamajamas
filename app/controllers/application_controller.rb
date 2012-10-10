c3ab2c12d1991c6e4ae265145a70e9c8e4564f82
ryane <ryanesc@gmail.com>
2012-10-10 14:53:17 -0400

    display confirmation instructions if user is not confirmed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :prompt_to_confirm_email_maybe
+
+  protected
+
+  def prompt_to_confirm_email_maybe
+    if user_signed_in? && !current_user.confirmed?
+      # this may get overwritten at times but that should be ok
+      flash[:notice] = render_to_string(partial: "shared/confirmation_instructions").html_safe
+    end
+  end
 end
