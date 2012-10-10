a9a14c1bba2b6a5f4f9362d98eb22ab66290c2fe
ryane <ryanesc@gmail.com>
2012-10-10 16:59:31 -0400

    show email confirmation instructions if it is > 48 hours since signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,15 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  # show a confirmation message if the user is not confirmed and it has
+  # been more than 48 hours since they signed up
   def prompt_to_confirm_email_maybe
     if user_signed_in? && !current_user.confirmed?
-      # this may get overwritten at times but that should be ok
-      flash[:notice] = render_to_string(partial: "shared/confirmation_instructions").html_safe
+      if Time.now.utc - User.first.confirmation_sent_at > 48.hours
+        if flash[:notice].blank?
+          flash[:notice] = render_to_string(partial: "shared/confirmation_instructions").html_safe
+        end
+      end
     end
   end
 end
