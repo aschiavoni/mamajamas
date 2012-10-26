50af495dc2f9c5c7a09b884a49e36c4db67e9208
ryane <ryanesc@gmail.com>
2012-10-26 15:42:10 -0400

    refactored progress indicator toggling into a reusable jquery plugin

---
+++ app/assets/javascripts/jquery.progressindicator.js
@@ -0,0 +1,40 @@
+// custom simple plugin to toggle a progress indicator
+//
+// Examples:
+// $("#signup-modal, #login-modal").progressIndicator('show');
+// $("#signup-modal, #login-modal").progressIndicator('hide');
+//
+// assumes markup like:
+//
+// <div class="progress-container">
+//   <button type="reset" class="button" id="bt-cancel">Cancel</button>
+//   <button type="submit" class="button" id="bt-log-in">Log in</button>
+//   <%= image_tag("progress-bar.gif", :class => "progress", :style => "display: none;") %>
+// </div>
+
+
+(function($){
+  var methods = {
+    show: function() {
+      var $progress = $(".progress-container .progress", this);
+      $progress.show().siblings().hide();
+      return this;
+    },
+    hide: function() {
+      var $progress = $(".progress-container .progress", this);
+      $progress.hide().siblings().show();
+      return this;
+    },
+  };
+
+  $.fn.progressIndicator = function( method ) {
+    if ( methods[method] ) {
+      return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+    } else if ( typeof method === 'object' || ! method ) {
+      return methods.init.apply( this, arguments );
+    } else {
+      $.error( 'Method ' +  method + ' does not exist on jQuery.tooltip' );
+    }
+  };
+
+})( jQuery );
