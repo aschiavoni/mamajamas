4c2ea7acea85cd8fd66dbc4c6750ce20fbddc9f9
ryane <ryanesc@gmail.com>
2012-10-26 20:05:15 -0400

    backbone infrastructure

---
+++ app/assets/javascripts/mamajamas.js
@@ -0,0 +1,13 @@
+window.Mamajamas = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    return console.log('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function() {
+  return Mamajamas.initialize();
+});
