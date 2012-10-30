487541cbf069b6f8aff48ed42a99635631c95b15
ryane <ryanesc@gmail.com>
2012-10-29 21:28:37 -0400

    relationship modeling

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,11 @@ class User < ActiveRecord::Base
   attr_accessible :login, :provider, :uid, :access_token, :access_token_expires_at
   attr_accessible :first_name, :last_name
 
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_relationships, foreign_key: "followed_id", class_name: "Relationship", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_relationships, source: :follower
+
   validates :username, presence: true, uniqueness: true
 
   # hook devise to support logging in by email or username
@@ -39,5 +44,17 @@ class User < ActiveRecord::Base
 
   def facebook
     @facebook ||= FacebookGraph.new(self)
+  end
+
+  def following?(other_user)
+    relationships.find_by_followed_id(other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by_followed_id(other_user.id).destroy
   end
 end
