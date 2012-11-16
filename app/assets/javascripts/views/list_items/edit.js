95cc473987ef391806bcc00f6fce611ecedebe83
ryane <ryanesc@gmail.com>
2012-11-15 19:35:59 -0500

    wired up showing the list item edit view when add is clicked

--- app/assets/javascripts/views/list_items/edit.js
+++ app/assets/javascripts/views/list_items/edit.js
@@ -2,22 +2,23 @@ Mamajamas.Views.ListItemEdit = Backbone.View.extend({
 
   tagName: 'tr',
 
-  initialize: function() {
-    console.log("initializing ListItemsEdit...");
-    this.$el.
-      addClass("prod").
-      addClass("prod-filled").
-      addClass("edit-mode");
-  },
+  className: "prod prod-filled edit-mode",
 
   events: {
-    "click .save-item.button": "save"
+    "click .save-item.button": "save",
+    "click .cancel-item.button": "cancel"
   },
 
   render: function(event) {
     var $template = Handlebars.compile($("#add-item-template").html());
     this.$el.html($template(this.model.toJSON()));
+
     return this;
+  },
+
+  setup: function() {
+    $("label", this.$el).inFieldLabels({ fadeDuration:200,fadeOpacity:0.55 });
+    $("#list_item_name", this.$el).focus();
   },
 
   save: function(event) {
@@ -26,5 +27,11 @@ Mamajamas.Views.ListItemEdit = Backbone.View.extend({
       console.log(data);
     });
     return false;
+  },
+
+  cancel: function(event) {
+    this.options.productType.$el.show();
+    this.$el.remove();
+    return true;
   }
 });
--- app/assets/javascripts/views/product_types/show.js
+++ app/assets/javascripts/views/product_types/show.js
@@ -4,16 +4,27 @@ Mamajamas.Views.ProductTypeShow = Backbone.View.extend({
 
   template: HandlebarsTemplates['product_types/show'],
 
-  initialize: function() {
-    this.$el.addClass("prod");
-  },
+  className: "prod",
 
   events: {
+    "click .add-item.button": "addItem",
   },
 
   render: function(event) {
     this.$el.html(this.template({ productType: this.model.toJSON() }));
     return this;
+  },
+
+  addItem: function(event) {
+    var addItem = new Mamajamas.Views.ListItemEdit({
+      model: new Mamajamas.Models.ListItem(),
+      productType: this
+    });
+
+    this.$el.after(addItem.render().$el);
+    this.$el.hide();
+    addItem.setup();
+
+    return false;
   }
 });
-
