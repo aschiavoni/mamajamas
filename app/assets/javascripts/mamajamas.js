2c47817595b3c5b363792c6869bd77db0bd6f526
ryane <ryanesc@gmail.com>
2012-11-25 17:25:37 -0500

    wire up notifications in the app initializer

--- app/assets/javascripts/mamajamas.js
+++ app/assets/javascripts/mamajamas.js
@@ -3,14 +3,24 @@ window.Mamajamas = {
   Collections: {},
   Views: {},
   Routers: {},
-  Context: {},
+  Context: {
+  },
   initialize: function() {
+    // initialize notifications
+    Mamajamas.Context.Notifications = new Mamajamas.Collections.Notifications();
+    var notifications = new Mamajamas.Views.NotificationsIndex({
+      collection: Mamajamas.Context.Notifications
+    });
+    notifications.render();
+
+    // follow page
     if ($("#follow-moms").length > 0) {
       new Mamajamas.Views.FriendPicker({
         el: '#follow-moms'
       });
     }
 
+    // list
     if ($("#my-list").length > 0) {
       new Mamajamas.Routers.ListItems();
       Backbone.history.start();
