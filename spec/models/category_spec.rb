882b7a1ddc13bd32e664bd1210c19c386ff63881
ryane <ryanesc@gmail.com>
2012-12-03 14:37:43 -0500

    category specs

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe Category do
-  pending
+
+  describe "slugs" do
+
+    it "should have slugged value" do
+      category = create(:category, name: "Category 1")
+      category.slug.should == "category-1"
+    end
+
+  end
+
 end
