51989f0e46d76c7783dc4badbc238dee2864225e
ryane <ryanesc@gmail.com>
2012-12-10 16:12:30 -0500

    just a little code cleanup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,14 @@
 class UsersController < ApplicationController
   def edit
-	@user = User.find(params[:id])
+    @user = User.find(params[:id])
   end
 
  def update
     @user = User.find(params[:id])
 
     respond_to do |format|
-
-	  #returns true if parameters are valid, otherwise false
       if @user.update_attributes(params[:user])
-		#if regular page request, redirects to user page
-        #format.html { redirect_to @user, notice: 'User was successfully updated.' }
-		format.html { render action: "edit", notice: 'User was successfully updated.' }
-		#if json request, no visible response
+        format.html { render action: "edit", notice: 'User was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
