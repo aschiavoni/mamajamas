4aa7bc4084c221b07b8c051ca888dd71e1bd6a1a
ryane <ryanesc@gmail.com>
2012-12-22 00:40:44 -0500

    simplified by category specs

--- spec/models/list_item_spec.rb
+++ spec/models/list_item_spec.rb
@@ -2,10 +2,6 @@ require 'spec_helper'
 
 describe ListItem do
 
-  let(:bathing_category) { create(:category, name: "bathing") }
-  let(:changing_category) { create(:category, name: "changing") }
-  let(:categories) { [ bathing_category, changing_category ] }
-
   describe "is valid" do
 
     it "must have a name" do
@@ -22,45 +18,15 @@ describe ListItem do
 
   describe "by category" do
 
-    before(:all) do
-      ListItem.delete_all
-      @b1 = create(:list_item, category_id: bathing_category.id)
-      @b2 = create(:list_item, category_id: bathing_category.id)
-      @c1 = create(:list_item, category_id: changing_category.id)
-      @c2 = create(:list_item, category_id: changing_category.id)
-      @all_list_items = [ @b1, @b2, @c1, @c2 ]
+    it "should query by category if specified" do
+      category = stub(:category, id: 1)
+      subject.class.should_receive(:where).with(category_id: category.id)
+      subject.class.by_category(category)
     end
 
-    describe "filtered by category" do
-
-      before(:all) do
-        @results = subject.class.by_category(bathing_category)
-      end
-
-      it "should only include list items in the bathing category" do
-        @results.should include(@b1)
-      end
-
-      it "should include all bathing list items" do
-        @results.size.should == 2 # @b1 and @b2
-      end
-
-      it "should not include list items in the changing category" do
-        @results.should_not include(@c1)
-      end
-
-    end
-
-    describe "filtered by nil category" do
-
-      before(:all) do
-        @results = subject.class.by_category(nil)
-      end
-
-      it "should include all list items" do
-        @results.size.should == @all_list_items.size
-      end
-
+    it "should not filter results if category is blank" do
+      subject.class.should_receive(:scoped)
+      subject.class.by_category(nil)
     end
 
   end
--- spec/models/product_type_spec.rb
+++ spec/models/product_type_spec.rb
@@ -2,50 +2,17 @@ require 'spec_helper'
 
 describe ProductType do
 
-  let(:bathing_category) { create(:category, name: "bathing") }
-  let(:changing_category) { create(:category, name: "changing") }
-  let(:categories) { [ bathing_category, changing_category ] }
-
   describe "by category" do
 
-    before(:all) do
-      @b1 = create(:product_type, category_id: bathing_category.id)
-      @b2 = create(:product_type, category_id: bathing_category.id)
-      @c1 = create(:product_type, category_id: changing_category.id)
-      @c2 = create(:product_type, category_id: changing_category.id)
-      @all_product_types = [ @b1, @b2, @c1, @c2 ]
+    it "should query by category if specified" do
+      category = stub(:category, id: 1)
+      subject.class.should_receive(:where).with(category_id: category.id)
+      subject.class.by_category(category)
     end
 
-    describe "filtered by category" do
-
-      before(:all) do
-        @results = subject.class.by_category(bathing_category)
-      end
-
-      it "should only include product types in the bathing category" do
-        @results.should include(@b1)
-      end
-
-      it "should include all bathing product types" do
-        @results.size.should == 2 # @b1 and @b2
-      end
-
-      it "should not include product types in the changing category" do
-        @results.should_not include(@c1)
-      end
-
-    end
-
-    describe "filtered by nil category" do
-
-      before(:all) do
-        @results = subject.class.by_category(nil)
-      end
-
-      it "should include all product types" do
-        @results.size.should == ProductType.count
-      end
-
+    it "should not filter results if category is blank" do
+      subject.class.should_receive(:scoped)
+      subject.class.by_category(nil)
     end
 
   end
