79537a3dee8c7912a144cd1700f06d3b2110a04a
ryane <ryanesc@gmail.com>
2013-01-25 17:58:01 -0500

    add tooltip to priority indicator

--- app/assets/javascripts/helpers/priority_indicator.js
+++ app/assets/javascripts/helpers/priority_indicator.js
@@ -1,17 +1,21 @@
 Handlebars.registerHelper('priorityIndicator', function(priority) {
   var className = null;
+  var title = null;
 
   switch(priority) {
     case 1:
       className = "priority-high";
+      title = "Get it";
       break;
     case 2:
       className = "priority-med";
+      title = "Consider it";
       break;
     default:
       className = "priority-low";
+      title = "Don't bother";
   }
 
-  var div = "<div class=\"priority-display " + className + "\">" + priority + "</div>";
+  var div = "<div class=\"priority-display " + className + "\" title=\"" + title + "\">" + priority + "</div>";
   return new Handlebars.SafeString(div);
 });
