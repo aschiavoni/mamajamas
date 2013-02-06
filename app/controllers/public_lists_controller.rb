db28a5295a9c8f71653da1836e159d8d481f3bfc
ryane <ryanesc@gmail.com>
2013-02-06 13:33:06 -0500

    use dynamic categories in public list

--- app/controllers/public_lists_controller.rb
+++ app/controllers/public_lists_controller.rb
@@ -1,11 +1,23 @@
 class PublicListsController < ApplicationController
   before_filter :find_public_list
+  before_filter :init_view
 
   def show
-    @page_id = "publist"
+    @categories = @list.categories.for_list
+
+    if params[:category].present?
+      @category = @categories.by_slug(params[:category]).first
+    else
+      @category = @categories.first
+    end
   end
 
   private
+
+  def init_view
+    @page_id = "publist"
+    @subheader = @list.title
+  end
 
   def find_public_list
     owner = User.find_by_username(params[:username])
