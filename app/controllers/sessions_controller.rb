890868d95a43860b122a550c8676f4d2443ea5dd
ryane <ryanesc@gmail.com>
2013-04-26 13:52:54 -0400

    restyled login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < Devise::SessionsController
+  before_filter :init_view
+
   def new
     self.resource = build_resource(nil, :unsafe => true)
 
@@ -19,5 +21,15 @@ class SessionsController < Devise::SessionsController
     set_flash_message(:notice, :signed_in) if is_navigational_format?
     (render(:js => "window.location=\"#{after_sign_in_path_for(resource)}\";") && return) if request.xhr?
     respond_with resource, :location => after_sign_in_path_for(resource)
+  end
+
+  private
+
+  def init_view
+    set_page_id "user-login"
+    set_subheader = "Login"
+    set_body_class "form-page"
+    hide_header
+    hide_progress_bar
   end
 end
