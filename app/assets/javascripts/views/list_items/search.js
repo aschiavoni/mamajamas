026d44b228c797407829741b4c854947d6130e3b
ryane <ryanesc@gmail.com>
2013-05-07 14:28:55 -0400

    begin wiring of search functionality

--- app/assets/javascripts/views/list_items/search.js
+++ app/assets/javascripts/views/list_items/search.js
@@ -9,10 +9,15 @@ Mamajamas.Views.ListItemSearch = Mamajamas.Views.Base.extend({
 
   events: {
     'click .bt-close': 'close',
+    'keyup #field-search': 'startSearch',
+    'blur #field-search': 'endSearch',
   },
 
   render: function() {
-    this.$el.html(this.template({})).show();
+    this.$el.html(this.template({})).show(function() {
+      $("label", this.$el).inFieldLabels({ fadeDuration:200,fadeOpacity:0.55 });
+      $("#field-search", this.$el).focus();
+    });
     return this;
   },
 
@@ -20,6 +25,14 @@ Mamajamas.Views.ListItemSearch = Mamajamas.Views.Base.extend({
     event.preventDefault();
     this.$el.remove();
     return false;
+  },
+
+  startSearch: function() {
+    this.$el.progressIndicator('show');
+  },
+
+  endSearch: function() {
+    this.$el.progressIndicator('hide');
   },
 
 });
