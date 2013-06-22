dd20b01dfc72e6758bdddbd9dd72b491aa2c8281
ryane <ryanesc@gmail.com>
2013-06-21 21:11:39 -0400

    whitespace

--- app/assets/javascripts/views/appAuth.js
+++ app/assets/javascripts/views/appAuth.js
@@ -1,4 +1,5 @@
 window.Mamajamas.Views.AppAuth = Backbone.View.extend({
+
   initialize: function() {
     this.model.on('facebook:disconnected', this.serverLogout, this);
 
@@ -17,37 +18,46 @@ window.Mamajamas.Views.AppAuth = Backbone.View.extend({
       el: '#complete-registration-modal'
     });
   },
+
   events: {
     "click #signup-link": "signup",
     "click #login-link": "login",
     "click #bt-fb-connect, #bt-fb-connect-s": "facebookLogin",
     "click #logout": "logout"
   },
+
   render: function(event) {
     return this;
   },
+
   signup: function() {
     this._signupModal.show();
     return false;
   },
+
   completeRegistration: function() {
     this._completeRegistrationModal.show();
     return false;
   },
+
   login: function() {
     this._loginModal.show();
     return false;
   },
+
   facebookLogin: function(event) {
     event.preventDefault();
     this.model.login();
     return false;
   },
+
   logout: function() {
     this.model.logout();
     return false;
   },
+
   serverLogout: function(fbresponse) {
     $("#server-logout").click();
-  }
+  },
+
 });
