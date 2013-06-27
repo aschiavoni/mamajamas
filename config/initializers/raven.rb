8d951795f89fb43daa2b8a82ebf6f562bd847875
ryane <ryanesc@gmail.com>
2013-06-26 22:30:43 -0400

    tweak sentry for staging environment

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,5 +1,7 @@
 require 'raven'
 
-Raven.configure do |config|
-  config.dsn = 'https://6eb791d810cd4282a27b316e4f0ff42e:b5a3271cf7a749b2be2999fad585a20e@app.getsentry.com/9536'
+if Rails.env.staging?
+  Raven.configure do |config|
+    config.dsn = 'https://6eb791d810cd4282a27b316e4f0ff42e:b5a3271cf7a749b2be2999fad585a20e@app.getsentry.com/9536'
+  end
 end
