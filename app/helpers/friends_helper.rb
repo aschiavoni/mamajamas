fa6eff96f247e51d1ac0b7703c9fd5dfaba316e1
ryane <ryanesc@gmail.com>
2013-07-18 10:58:48 -0400

    clean up friends page when there are missing friend types

--- app/helpers/friends_helper.rb
+++ app/helpers/friends_helper.rb
@@ -1,2 +1,13 @@
 module FriendsHelper
+  def has_facebook_friends?
+    @fb_friends.size > 0
+  end
+
+  def has_recommended_friends?
+    @recommended_friends.size > 0
+  end
+
+  def has_any_friends?
+    has_facebook_friends? || has_recommended_friends?
+  end
 end
