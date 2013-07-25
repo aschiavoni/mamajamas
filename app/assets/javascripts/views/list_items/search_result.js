d6569c8ed5f2c75162944ffe66331d013db47365
ryane <ryanesc@gmail.com>
2013-07-25 12:41:09 -0400

    show proper rating on search modal

--- app/assets/javascripts/views/list_items/search_result.js
+++ app/assets/javascripts/views/list_items/search_result.js
@@ -14,6 +14,8 @@ Mamajamas.Views.ListItemSearchResult = Mamajamas.Views.Base.extend({
 
   render: function() {
     this.$el.html(this.template(this.model.toJSON()));
+    this.updateRating("mamajamas_rating", ".detail-rating-mj", "egg");
+    this.updateRating("rating", ".detail-rating-am", "star");
     return this;
   },
 
@@ -26,6 +28,25 @@ Mamajamas.Views.ListItemSearchResult = Mamajamas.Views.Base.extend({
   doNothing: function(event) {
     event.preventDefault();
     return false;
+  },
+
+  updateRating: function(ratingName, ratingContainerSelector, starClassName) {
+    var itemRating = this.model.get(ratingName);
+    if (itemRating) {
+      itemRating = parseFloat(itemRating);
+      var $stars = $("." + starClassName, $(ratingContainerSelector, this.$el));
+      $($stars).each(function(idx, el) {
+        var $star = $(el);
+        var starRating = parseFloat($star.data("rating"));
+        if (starRating > (itemRating + 0.5))
+          return false;
+        if (starRating <= itemRating)
+          $star.addClass(starClassName + "-full");
+        else if ((starRating - 0.5) <= itemRating) {
+          $star.addClass(starClassName + "-half");
+        }
+      });
+    }
   },
 
 });
