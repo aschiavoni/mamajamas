5f20e7003fd857fcebafe2f8c842f04bb05401f8
ryane <ryanesc@gmail.com>
2013-08-01 12:24:49 -0400

    still trying to get specs to work

--- spec/features/visitor_signs_up_spec.rb
+++ spec/features/visitor_signs_up_spec.rb
@@ -3,30 +3,35 @@ require 'spec_helper'
 feature "Visitor signs up", js: true do
 
   scenario "with valid name, email and password" do
-    @tempuser = build(:user)
-    sign_up_with @tempuser.email, "really!good$password"
+    VCR.use_cassette('signup/email') do
+      @tempuser = build(:user)
+      sign_up_with @tempuser.email, "really!good$password"
 
-    expect(page).to have_selector("#logout")
-    expect(page).to have_content("My Account")
-    expect(page).to have_content("Take the quiz")
-    current_path.should == friends_path
+      expect(page).to have_selector("#logout")
+      expect(page).to have_content("My Account")
+      expect(page).to have_content("Take the quiz")
+      current_path.should == friends_path
+    end
   end
 
   scenario "with invalid email" do
     @tempuser = build(:user)
     sign_up_with nil, "really!good$password"
+    sleep 0.5
     expect(page).to have_selector(".status-msg.error", text: "email")
   end
 
   scenario "with invalid password" do
     @tempuser = build(:user)
     sign_up_with @tempuser.email, nil
+    sleep 0.5
     expect(page).to have_selector(".status-msg.error", text: "password")
   end
 
   scenario "with invalid name" do
     @tempuser = build(:user)
     sign_up_with @tempuser.email, "really!good$password", nil
+    sleep 0.5
     expect(page).to have_selector(".status-msg.error", text: "name")
   end
 
