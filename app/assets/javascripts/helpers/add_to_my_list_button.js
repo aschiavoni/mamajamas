e159e3df3bcfc84f71510c2a888c4145fd1ae3f7
ryane <ryanesc@gmail.com>
2013-08-12 21:57:25 -0400

    add to my list button links to list in a new window

--- app/assets/javascripts/helpers/add_to_my_list_button.js
+++ app/assets/javascripts/helpers/add_to_my_list_button.js
@@ -1,6 +1,6 @@
 Handlebars.registerHelper('addToMyListButton', function(priority) {
   if (Mamajamas.Context.User && Mamajamas.Context.User.get('has_list')) {
-    var btn = "<a href=\"#\" class=\"button bt-color bt-add\" title=\"Add to my list\"><span class=\"ss-plus\"></span><strong>Add</strong></a>"
+    var btn = "<a href=\"/list\" target=\"_blank\" class=\"button bt-color bt-add\" title=\"Add to my list\"><span class=\"ss-plus\"></span><strong>Add</strong></a>"
     return new Handlebars.SafeString(btn);
   }
 });
