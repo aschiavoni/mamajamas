43caa07c5ae897e536d029636f8bd7fc5e18b894
ryane <ryanesc@gmail.com>
2013-08-24 10:49:30 -0400

    implement new public list sidebar follow button

--- app/assets/javascripts/views/public_lists/sidebar.js
+++ app/assets/javascripts/views/public_lists/sidebar.js
@@ -1,57 +1,59 @@
 Mamajamas.Views.PublicListSidebar = Backbone.View.extend({
 
+  $followButton: null,
+
   initialize: function() {
+    this.$followButton = $("#bt-follow", this.$el);
   },
 
   events: {
-    "click #bt-follow .follow": "follow",
-    "click #bt-follow .unfollow": "unfollow",
+    "click #bt-follow": "toggleRelationship",
   },
 
   render: function() {
     return this;
   },
 
-  follow: function(event) {
+  toggleRelationship: function(event) {
     event.preventDefault();
+    if (this.$followButton.hasClass("bt-active")) {
+      this.unfollow();
+    } else {
+      this.follow();
+    }
+    return false;
+  },
 
+  follow: function() {
     var _view = this;
-    var btnContainer = $('#bt-follow', this.$el);
-    var followedId = btnContainer.data("owner-id");
+    var followedId = this.$followButton.data("owner-id");
 
     var data = { relationship: { followed_id: followedId }, format: "json" };
     $.post('/relationships', data, function(response) {
-      btnContainer.data("relationship-id", response.relationship_id);
-      _view.toggleFollowButton($(event.target));
+      _view.$followButton.data("relationship-id", response.relationship_id);
+      _view.showFollowingButton();
     });
-
-    return false;
   },
 
-  unfollow: function(event) {
-    event.preventDefault();
-
+  unfollow: function() {
     var _view = this;
-    var btnContainer = $('#bt-follow', this.$el);
-    var relationshipId = btnContainer.data("relationship-id");
+    var relationshipId = this.$followButton.data("relationship-id");
 
     var data = { _method: "delete" };
     $.post("/relationships/" + relationshipId, data, function(response) {
-      btnContainer.data("relationship-id", null);
-      _view.toggleFollowButton($(event.target));
+      _view.$followButton.data("relationship-id", null);
+      _view.showFollowButton();
     });
-
-    return false;
   },
 
-  toggleFollowButton: function(btn) {
-    if (btn.html() == "Follow") {
-      btn.removeClass("follow").addClass("unfollow");
-      btn.html("Unfollow");
-    } else {
-      btn.removeClass("unfollow").addClass("follow");
-      btn.html("Follow");
-    }
+  showFollowButton: function() {
+    this.$followButton.removeClass("bt-active").empty();
+    this.$followButton.html("<span class=\"ss-check\"></span>Follow");
+  },
+
+  showFollowingButton: function() {
+    this.$followButton.addClass("bt-active").empty();
+    this.$followButton.html("<span class=\"bt-text1\"><span class=\"ss-check\"></span>Following</span><em class=\"bt-text2\"><span class=\"ss-delete\"></span>Unfollow</em>");
   },
 
 });
