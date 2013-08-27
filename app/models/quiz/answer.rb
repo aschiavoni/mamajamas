d42ae9f14c98d59a405286bb11d8732b237f1afa
ryane <ryanesc@gmail.com>
2013-08-27 08:00:55 -0400

    can query most recent answers

--- app/models/quiz/answer.rb
+++ app/models/quiz/answer.rb
@@ -12,4 +12,17 @@ class Quiz::Answer < ActiveRecord::Base
       answers: answers
     })
   end
+
+  def self.most_recent_answers(user_id)
+    # TODO: can this query be composed via arel?
+    find_by_sql([ %q{
+      select quiz_answers.* from quiz_answers
+      join (
+        select max(created_at) mt, question from quiz_answers group by question
+      ) md on (
+        md.question = quiz_answers.question and md.mt = quiz_answers.created_at
+      )
+      where quiz_answers.user_id = ?
+      }, user_id ])
+  end
 end
