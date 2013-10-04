5bce514d66d2268886f9a120e1ac75865801cc62
ryane <ryanesc@gmail.com>
2013-10-04 17:08:37 -0400

    change copy on add to my list button on public list page

--- app/assets/javascripts/helpers/add_to_my_list_button.js
+++ app/assets/javascripts/helpers/add_to_my_list_button.js
@@ -1,6 +1,6 @@
 Handlebars.registerHelper('addToMyListButton', function(priority) {
   if (Mamajamas.Context.User && Mamajamas.Context.User.get('has_list')) {
-    var btn = "<a href=\"/list/all\" target=\"_blank\" class=\"button bt-color bt-add\" title=\"Add to my list\"><span class=\"ss-plus\"></span><strong>Add</strong></a>"
+    var btn = "<a href=\"/list/all\" target=\"_blank\" class=\"button bt-color bt-add\" title=\"Add to my list\"><span class=\"ss-plus\"></span><strong>Add to my list</strong></a>"
     return new Handlebars.SafeString(btn);
   }
 });
