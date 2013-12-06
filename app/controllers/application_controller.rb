cfeb487c1f3143450f62d1b34969b71a28ae2963
ryane <ryanesc@gmail.com>
2013-12-06 15:50:37 -0500

    turn off basic auth temporarily

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter :require_basic_auth_maybe
+  # before_filter :require_basic_auth_maybe
   before_filter :set_meta
 
   # Convenience accessor for flash[:error]
