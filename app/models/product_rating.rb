29c7e6505ff2511d371af4443e1c911fa602842f
ryane <ryanesc@gmail.com>
2014-02-07 14:47:54 -0500

    can get a list of suggestable vendor ids from recommended and rated products

--- app/models/product_rating.rb
+++ app/models/product_rating.rb
@@ -1,3 +1,15 @@
 class ProductRating < ActiveRecord::Base
   attr_accessible :rating, :rating_count, :vendor, :vendor_id
+
+  def self.suggestable_vendor_ids(product_type)
+    join = %Q{
+      INNER JOIN list_items ON list_items.vendor_id = product_ratings.vendor_id
+    }
+    self.joins(join).
+      where(["list_items.placeholder = ?", false]).
+      where(["list_items.product_type_id = ?", product_type.id]).
+      where(["product_ratings.rating_count >= 5"]).
+      # where(["product_ratings.rating >= 4.0"]).
+      select("DISTINCT product_ratings.vendor_id").map(&:vendor_id)
+  end
 end
--- app/models/recommended_product.rb
+++ app/models/recommended_product.rb
@@ -2,4 +2,8 @@ class RecommendedProduct < ActiveRecord::Base
   belongs_to :product_type
   attr_accessible :product_type_id, :name, :image_url
   attr_accessible :link, :tag, :vendor, :vendor_id
+
+  def self.suggestable_vendor_ids(product_type)
+    self.where(product_type_id: product_type.id).pluck(:vendor_id)
+  end
 end
