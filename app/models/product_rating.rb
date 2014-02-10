6a49d1ad9ab8465f2992fdfeeddd6222032296a8
ryane <ryanesc@gmail.com>
2014-02-10 10:09:17 -0500

    suggestable products are ones that have at least an avg rating of 3

--- app/models/product_rating.rb
+++ app/models/product_rating.rb
@@ -9,7 +9,7 @@ class ProductRating < ActiveRecord::Base
       where(["list_items.placeholder = ?", false]).
       where(["list_items.product_type_id = ?", product_type.id]).
       where(["product_ratings.rating_count >= 5"]).
-      where(["product_ratings.rating >= 4.0"]).
+      where(["product_ratings.rating >= 3.0"]).
       select("DISTINCT product_ratings.vendor_id").map(&:vendor_id)
   end
 end
