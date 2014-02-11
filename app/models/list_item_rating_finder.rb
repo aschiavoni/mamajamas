df37f8c843fecbceeb7d4ec424dec410dfefc7b9
ryane <ryanesc@gmail.com>
2014-02-11 16:06:17 -0500

    ignore item with 0 rating when calculating rating averages

--- app/models/list_item_rating_finder.rb
+++ app/models/list_item_rating_finder.rb
@@ -2,6 +2,6 @@ class ListItemRatingFinder
   def self.find(vendor_id, vendor)
     list_items = ListItem.where(vendor: vendor, vendor_id: vendor_id)
     return [] if list_items.blank? || list_items.empty?
-    list_items.map(&:rating).reject { |r| r.blank? }
+    list_items.map(&:rating).reject { |r| r.blank? || r.zero? }
   end
 end
