287f6f960b4268a7e7f6d657eb2b0a3ea5b645c4
ryane <ryanesc@gmail.com>
2014-09-25 14:58:53 -0400

    don't rewrite amazon links

--- app/assets/javascripts/models/affiliate_link.js
+++ app/assets/javascripts/models/affiliate_link.js
@@ -3,10 +3,16 @@ function AffiliateLink(url) {
 
   var AFFILIATE_URL = "http://go.redirectingat.com/?";
   var AFFILIATE_PUBLISHER_ID = "123X456"
+  var AFFILIATE_AMAZON_ID = "mamajamas-20"
 
   that.url = url;
 
   this.generateUrl = function(url) {
+    // don't rewrite amazon urls that include our
+    // naive check
+    if (/amazon\.com/i.test(url) && new RegExp(AFFILIATE_AMAZON_ID, "i").test(url))
+      return url;
+
     var params = {
       id: AFFILIATE_PUBLISHER_ID,
       url: url
--- spec/javascripts/affiliate_link_spec.js
+++ spec/javascripts/affiliate_link_spec.js
@@ -9,4 +9,20 @@ describe("AffiliateLink", function() {
     var al = new AffiliateLink(url);
     expect(al.affiliateUrl).toEqual(affUrl)
   });
+
+  it("does not generate an affiliate url for amzn affiliate urls", function() {
+    var url =
+      "http://www.amazon.com/Keekaroo-Peanut-Diaper-Changer-Solid/dp/B009A7VTYA%3FSubscriptionId%3DAKIAIXAEIBVZBZEU56MQ%26tag%3Dmamajamas-20%26linkCode%3Dxm2%26camp%3D2025%26creative%3D165953%26creativeASIN%3DB009A7VTYA";
+
+    var al = new AffiliateLink(url);
+    expect(al.affiliateUrl).toEqual(url);
+  });
+
+  it("does generate an affiliate url for amzn non-affiliate urls", function() {
+    var url = "http://www.amazon.com/Keekaroo-Peanut-Diaper-Changer-Solid/dp/B009A7VTYA%3FSubscriptionId%3DAKIAIXAEIBVZBZEU56MQ%26tag%3Dsommaff-20%26linkCode%3Dxm2%26camp%3D2025%26creative%3D165953%26creativeASIN%3DB009A7VTYA";
+    var affUrl = "http://go.redirectingat.com/?id=123X456&url=http%3A%2F%2Fwww.amazon.com%2FKeekaroo-Peanut-Diaper-Changer-Solid%2Fdp%2FB009A7VTYA%253FSubscriptionId%253DAKIAIXAEIBVZBZEU56MQ%2526tag%253Dsommaff-20%2526linkCode%253Dxm2%2526camp%253D2025%2526creative%253D165953%2526creativeASIN%253DB009A7VTYA";
+
+    var al = new AffiliateLink(url);
+    expect(al.affiliateUrl).toEqual(affUrl);
+  });
 });
