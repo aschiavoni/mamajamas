21b191278ca4b1242b9190053ca222cc0cfcd7a7
ryane <ryanesc@gmail.com>
2014-10-09 11:22:44 -0400

    user can edit product type when editing a list item

--- app/assets/javascripts/views/list_items/edit.js
+++ app/assets/javascripts/views/list_items/edit.js
@@ -43,6 +43,16 @@ Mamajamas.Views.ListItemEdit = Mamajamas.Views.ListItem.extend({
     }
 
     // subviews
+    var allowChange = Mamajamas.Context.List.get('category_id');
+    var productTypeView = new Mamajamas.Views.ProductType({
+      model: {
+        listItem: this.model,
+        list: Mamajamas.Context.List,
+        allowChange: allowChange
+      }
+    });
+    $('.prod-category', this.$el).prepend(productTypeView.render().$el);
+
     var ratingView = new Mamajamas.Views.ListItemRating({
       model: this.model
     });
