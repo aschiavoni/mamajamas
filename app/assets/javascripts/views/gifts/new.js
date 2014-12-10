8a20938136d2af6cf419f8d0dc3e838a14b12181
ryane <ryanesc@gmail.com>
2014-12-09 21:26:09 -0500

    change gift button to continue after clicking

--- app/assets/javascripts/views/gifts/new.js
+++ app/assets/javascripts/views/gifts/new.js
@@ -2,6 +2,8 @@ Mamajamas.Views.GiftNew = Backbone.View.extend({
 
   _tabs: null,
 
+  _visitedProductSite: false,
+
   initialize: function() {
     var _view = this;
     _view._tabs = $(".tabs").accessibleTabs({
@@ -15,6 +17,7 @@ Mamajamas.Views.GiftNew = Backbone.View.extend({
     _view.inFieldLabels();
 
     $('#step1-continue', _view.$el).click(_view.step1Continue.bind(_view));
+    $('#step2-continue', _view.$el).click(_view.step2Continue.bind(_view));
   },
 
   events: {
@@ -27,6 +30,23 @@ Mamajamas.Views.GiftNew = Backbone.View.extend({
   step1Continue: function(event) {
     event.preventDefault();
     this._showTab(1);
+    return false;
+  },
+
+  step2Continue: function(event) {
+    if (!this._visitedProductSite) {
+
+      _.delay(function() {
+        var $button = $(event.currentTarget);
+        $button.html("Continue");
+        $button.blur();
+      }, 1000, event);
+
+      this._visitedProductSite = true;
+      return true;
+    }
+
+    this._showTab(2);
     return false;
   },
 
